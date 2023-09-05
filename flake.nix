64596c8b7be778c9b614e789384d51a1c54881da
Domen Kožar <domen@dev.si>
2023-09-05 13:27:07 +0100

    fix eval

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
 
-  outputs = { git, ... }: {
+  outputs = { nixpkgs, ... }: {
     packages.x86_64-linux.git = nixpkgs.legacyPackages.git;
   };
 }
