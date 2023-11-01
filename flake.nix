d79960891aa4c77983be1db39e880767530346f4
Domen Kožar <domen@dev.si>
2023-11-01 15:56:12 +0000

    add more inputs/systems

--- flake.nix
+++ flake.nix
@@ -2,14 +2,17 @@
   description = "test description";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+  inputs.flake-utils.url = github:numtide/flake-utils;
 
   outputs = { nixpkgs, ... }: {
     packages.x86_64-linux.git = nixpkgs.legacyPackages.x86_64-linux.git;
+    packages.x86_64-darwin.git = nixpkgs.legacyPackages.x86_64-darwin.git;
     packages.x86_64-linux.vim = nixpkgs.legacyPackages.x86_64-linux.vim;
+   
 
     checks.x86_64-linux.git = nixpkgs.legacyPackages.x86_64-linux.git;
 
-    apps.x86_64-linux.default = { type = "app"; program = "..."; };
+    apps.x86_64-linux.default = { type = "app"; program = "/nix/store/a"; };
     
     overlays."myoverlay" = final: prev: { };
 
